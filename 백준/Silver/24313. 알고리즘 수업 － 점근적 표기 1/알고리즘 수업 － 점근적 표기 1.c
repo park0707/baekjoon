3a86b272896d441702f7ef97309311470438fca9
park0707 <102636801+park0707@users.noreply.github.com>
2025-01-21 22:59:54 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+int main(void)
+{   
+    int a1, a0, c, n0;
+    scanf("%d %d", &a1, &a0);
+    scanf("%d", &c);
+    scanf("%d", &n0);
+    if(a1*n0+a0 <= c*n0 && c>= a1)
+        printf("1");
+    else
+        printf("0");
+
+    return 0;
+}
