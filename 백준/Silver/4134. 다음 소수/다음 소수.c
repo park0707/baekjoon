a0aa2301fb6d5e613336b3b89563fe53595d9a1c
park0707 <102636801+park0707@users.noreply.github.com>
2025-02-13 18:29:12 +0900

    Update 다음 소수.c

--- 백준/Silver/4134. 다음 소수/다음 소수.c
+++ 백준/Silver/4134. 다음 소수/다음 소수.c
@@ -1,40 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-
-// 소수 판별 함수
-int is_prime(long long n) {
-    if (n < 2)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (long long i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    }
-    return 1;
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+int check(long long n)
+{
+	
+	
+	if (n < 2)
+		return 0;
+	else if (n == 2 || n == 3)
+		return 1;
+	else if (n % 2 == 0 || n % 3 == 0)
+		return 0;
+	else
+	{
+		for (long long i = 5; i*i <= n; i+=6)
+		{
+			if (n % i == 0 || n%(i+2) == 0)
+				return 0;
+		}
+	}
+	return 1;
+}
+void find(long long n)
+{
+	if (n < 2)
+	{
+		printf("%lld\n", 2);
+		return;
+	}
+	while (!check(n))
+	{
+		n++;
+	}
+	printf("%lld\n", n);
+}
+int main(void)
+{
+	long long how, n,max,min;
+	scanf("%lld", &how);
+	long long* arr = malloc(how * sizeof(long long));
+	for (int i = 0; i < how; i++)
+	{
+		scanf("%lld", &arr[i]);
+		find(arr[i]);
+	}
+	
+    free(arr);
+	return 0;
 }
 
-// 다음 소수 찾기 함수
-void find_next_prime(long long n) {
-    if (n <= 1) {
-        printf("2\n");
-        return;
-    }
-    while (!is_prime(n)) {
-        n++;
-    }
-    printf("%lld\n", n);
-}
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        long long n;
-        scanf("%lld", &n);
-        find_next_prime(n);
-    }
-    return 0;
-}
