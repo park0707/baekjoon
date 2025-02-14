f1b2303662e63fe32b25b6adb38ad62e15abf66f
park0707 <102636801+park0707@users.noreply.github.com>
2025-02-14 17:14:22 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#define ll long long 
+int main(void)
+{
+	ll how;
+	scanf("%lld", &how);
+	ll r = (ll)sqrt(how);
+	printf("%lld", r);
+	
+
+	return 0;
+}
+
